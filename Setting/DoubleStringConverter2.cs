b8c9cd0d926af0a20ccc2651a0340236d415678f
shigobu <shigobu@shigobu.com>
2021-09-01 22:36:51 +0900

    設定アプリ追加、画面作成

---
+++ Setting/DoubleStringConverter2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Setting
+{
+    class DoubleStringConverter2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is double doubleValue))
+            {
+                return "";
+            }
+            return doubleValue.ToString("F2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ Setting/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Setting
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+#if x64
+            string bitStr = "64bit版";
+#else
+            string bitStr = "32bit版";
+#endif
+            this.Title += bitStr;
+        }
+
+        /// <summary>
+        /// キャンセルボタン押下時のイベントハンドラ
+        /// </summary>
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
