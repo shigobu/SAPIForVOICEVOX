0eed0a752ea5dc8645dd4f1cd1274c0076cfb47c
shigobu <shigobu@shigobu.com>
2021-09-11 23:20:18 +0900

    全般タブのバインディング作成

---
+++ Setting/Converter/ParameterValueModeToBool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Setting
+{
+    class ParameterValueModeToBool : IValueConverter
+    {
+        // ConverterParameterをEnumに変換するメソッド
+        private ParameterValueMode ConvertFromConverterParameter(object parameter)
+        {
+            string parameterString = parameter as string;
+            return (ParameterValueMode)Enum.Parse(typeof(ParameterValueMode), parameterString);
+        }
+
+        #region IValueConverter メンバー
+        // Enum → bool
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // XAMLに定義されたConverterParameterをEnumに変換する
+            ParameterValueMode parameterValue = ConvertFromConverterParameter(parameter);
+
+            // ConverterParameterとバインディングソースの値が等しいか？
+            return parameterValue.Equals(value);
+        }
+
+        // bool → Enum
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // true→falseの変化は無視する
+            // ※こうすることで、選択されたラジオボタンだけをデータに反映させる
+            if (!(bool)value)
+                return System.Windows.DependencyProperty.UnsetValue;
+
+            // ConverterParameterをEnumに変換して返す
+            return ConvertFromConverterParameter(parameter);
+        }
+        #endregion    
+    }
+}
